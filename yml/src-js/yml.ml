0b8e5f91314bd09d1efafc78902ccc48b0d61c05
Patrick Ferris <pf341@patricoferris.com>
2021-04-25 01:23:39 +0100

    move js into yml directory

--- yml/src-js/yml.ml
+++ yml/src-js/yml.ml
@@ -8,10 +8,12 @@ type t =
   | `A of t list
   | `O of (string * t) list ]
 
+let schema = Jsyaml.Schema.json
+
 let of_string s : t res =
-  match Jsyaml.of_string s with
+  match Jsyaml.(of_string ~opts:(load_opts ~schema ()) s) with
   | Error (`YAMLException msg) -> Error (`Msg msg)
   | Error (`Msg _) as e -> e
   | Ok _ as ok -> ok
 
-let to_string (t : t) = Ok (Jsyaml.to_string t)
+let to_string (t : t) = Ok Jsyaml.(to_string ~opts:(dump_opts ~schema ()) t)
